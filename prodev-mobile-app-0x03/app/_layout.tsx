1d343a4e080fc7869781428a9117a82dbe825211
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-11-01 15:04:48 +0300

    Fix _layout.tsx - hide header & add SafeAreaProvider

--- prodev-mobile-app-0x03/app/_layout.tsx
+++ prodev-mobile-app-0x03/app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </SafeAreaProvider>
+  );
 }
